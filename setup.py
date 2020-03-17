3cad071c3d7fee64ec6131717e91ed1fa0c5a831
Md Nazrul Islam <connect2nazrul@gmail.com>
2020-03-18 00:48:15 +0600

    Back to development: 0.5.2

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ docs_requirements = [
 
 setup(
     name="fhirpath",
-    version="0.5.1.dev0",
+    version="0.5.2.dev0",
     author="Md Nazrul Islam",
     author_email="email2nazrul@gmail.com",
     classifiers=[
